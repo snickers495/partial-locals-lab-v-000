c807856d7d9e67fc38b1e232c58ce555e36a5b8d
Lisa Lee <snickers495@gmail.com>
2017-12-05 22:13:39 +0000

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,9 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(arg)
+    Student.all.map {|s| s if s.name.include?(arg.titleize)}.compact
+
+  end
 end
