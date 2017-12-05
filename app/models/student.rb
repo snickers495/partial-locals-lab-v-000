815375ac9a122a36c25b0959d6cc06328b10a07c
Lisa Lee <snickers495@gmail.com>
2017-12-05 21:55:22 +0000

    automatically backed up by learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -14,7 +14,7 @@ class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
 
-  def search(arg)
+  def self.search(arg)
     if arg == ""
       return self.all
     else
